e7f68ef428379dfa847bb0c55265dff04df8e3ea
GrecoLucas <up202208296@up.pt>
2024-10-16 09:23:52 +0100

    algumas funcoes

--- TP1.hs
+++ TP1.hs
@@ -8,7 +8,7 @@ import Data.Ord (comparing)
 
 -- Uncomment the some/all of the first three lines to import the modules, do not change the code of these lines.
 
-type City = String
+type City = Int
 type Path = [City]
 type Distance = Int
 
@@ -25,6 +25,7 @@ areAdjacent :: RoadMap -> City -> City -> Bool
 areAdjacent [] _ _ = False
 areAdjacent ((a,b,_):xs) c1 c2 
     | (c1 == a && c2 == b) || (c1 == b && c2 == a) = True
+    | c1 == c2 = True
     | otherwise = areAdjacent xs c1 c2
 
 -- if the first element its equal to one of the input cities, and the second element its equal to the other input city, return the distance
@@ -92,10 +93,10 @@ tspBruteForce = undefined -- only for groups of 3 people; groups of 2 people: do
 
 -- Some graphs to test your work
 gTest1 :: RoadMap
-gTest1 = [("7","6",1),("8","2",2),("6","5",2),("0","1",4),("2","5",4),("8","6",6),("2","3",7),("7","8",7),("0","7",8),("1","2",8),("3","4",9),("5","4",10),("1","7",11),("3","5",14),("7","5",14),("8","3",15)]
+gTest1 = [(7,6,1),(8,2,2),(6,5,2),(0,1,4),(2,5,4),(8,6,6),(2,3,7),(7,8,7),(0,7,8),(1,2,8),(3,4,9),(5,4,10),(1,7,11),(3,5,14),(7,5,14),(8,3,15)]
 
 gTest2 :: RoadMap
-gTest2 = [("0","1",10),("0","2",15),("0","3",20),("1","2",35),("1","3",25),("2","3",30)]
+gTest2 = [(0,1,10),(0,2,15),(0,3,20),(1,2,35),(1,3,25),(2,3,30)]
 
 gTest3 :: RoadMap -- unconnected graph
-gTest3 = [("0","1",4),("2","3",2)]
+gTest3 = [(0,1,4),(2,3,2)]
